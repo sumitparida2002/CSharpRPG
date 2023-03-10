fe47cc4c7b7262662115d3cf29f0b271820d618b
sumitparida2002 <80693203+sumitparida2002@users.noreply.github.com>
2023-03-10 14:01:45 -0500

    Created Controller for player model

--- SOSCRPG/Engine/Models/Player.cs
+++ SOSCRPG/Engine/Models/Player.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace Engine.Models
 {
-    internal class Player
+    public class Player
     {
+        public string Name { get; set; }
+        public string CharacterClass { get; set; }
+        public int HitPoints { get; set; }
+        public int ExperiencePoints { get; set; }
+        public int Level { get; set; }
+        public int Gold { get; set; }
     }
 }
---
+++ SOSCRPG/Engine/ViewModels/GameSession.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    internal class GameSession
+    {
+        Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Scott";
+            CurrentPlayer.Gold = 1000000;
+
+
+        }
+
+    }
+}
